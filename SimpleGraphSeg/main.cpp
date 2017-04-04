554edb592225ce45d533c8d17ae74313569cec70
John W. Miller <john.w.millr@gmail.com>
2017-04-04 15:34:09 -0500

    Add the first files for a simple attempt at using the graph library for segmentation

---
+++ SimpleGraphSeg/main.cpp
@@ -0,0 +1,17 @@
+// Main file for the SimpleGraphSeg project, meant to test out the graph segmentation library
+
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	// Must supply the name of the image we're going to segment
+	if(argc != 1){
+		std::cout << "USAGE: <File to segment>" << std::endl;
+		return EXIT_FAILURE
+	}
+
+	// Okay, get to work...
+	
+
+}
